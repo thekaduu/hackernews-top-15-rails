ebedf74adb8c9ebd315f18c6514cf9c0d3e78928
Eduardo Hertz <eduardohertz@gmail.com>
2020-01-15 15:53:04 -0300

    Configure action cable to work in development properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+
   resources :stories, only: %[index] do
     resources :relevant_comments, only: %i[index], constraints: { format: 'js' }
   end
